0a37b1535e10650489a6086851ab1ad1ff7a7ea5
Gillian <gillian.gonzales@ocsbstudent.ca>
2018-03-09 14:24:52 -0500

    Initial commit

---
+++ main.lua
@@ -0,0 +1,13 @@
+-----------------------------------------------------------------------------------------
+--
+-- main.lua
+--
+-- Created By Gillian Gonzales
+-- Created On March 09 2018
+--
+-- This program will calculate the price for pizza
+-----------------------------------------------------------------------------------------
+
+-- Change background colour
+display.setDefault( "background", 1, 1, 1 )
+
